759615925bbfcbe87f8537b62239f81c97bac29e
Iris <iriscontado@gmail.com>
2026-07-09 21:07:12 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import WaterCursor from "./components/Effects/WaterCursor";
 import RippleEffect from "./components/Effects/RippleEffect";
 
 import "./styles/globals.css";
